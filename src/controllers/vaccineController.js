6fc8c374f903e9f05f81491513af5cd59a501323
MartineBordon26 <martinebordon26@gmail.com>
2024-08-17 20:10:27 -0300

    add function save vaccine

--- src/controllers/vaccineController.js
+++ src/controllers/vaccineController.js
@@ -1,9 +1,11 @@
 const vaccines = require('../data/vaccine.json')
 const path = require('path');
 const { validationResult } = require("express-validator");
-
+const fs = require('fs');
+const vaccineJSON = path.join(__dirname, '../data/vaccine.json');
 //const usersJSON = path.join(__dirname, '../data/vaccine.json');
 //console.log({allVaccine})
+
 
 const vaccineController = {
     index: (req, res) =>{
@@ -13,12 +15,41 @@ const vaccineController = {
         res.render(path.resolve('./', './src/views/vaccine/createVaccine'))
     },
     save: (req, res) =>{
-        console.log("body ", req.body)
         const errors = validationResult(req);
         
         if (!errors.isEmpty()) {
             return res.render(path.resolve('./', './src/views/vaccine/createVaccine'), {errors: errors.mapped(), oldData: req.body});
         }
+
+        
+
+        let {commercialName, laboratory, vaccineType, originCountry} = req.body;
+
+        console.log({commercialName})
+
+        const generateId = () =>  {
+            let allVaccines = JSON.parse(fs.readFileSync(vaccineJSON, {encoding: 'utf-8'}));
+            let lastVaccine = allVaccines.pop();
+            if (lastVaccine) {
+                return lastVaccine.id + 1;
+            }
+            return 1;
+        }
+
+        let newVeccine = {
+            id: generateId(),
+            nombre_comercial: commercialName,
+            laboratory: laboratory,
+            vaccineType: vaccineType,
+            originCountry: originCountry
+        }
+
+        vaccines.push(newVeccine);
+        fs.writeFileSync(vaccineJSON, JSON.stringify(vaccines, null, ' '));
+
+
+
+
         res.redirect('/vaccines');
     },
 }
